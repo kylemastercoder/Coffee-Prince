8e6b68d784938313c5aac6d9dccfffe3b0fc2e8a
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2024-11-08 21:34:51 +0800

    fixed user issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,41 @@ import MenuSection from "@/components/globals/landing-page/menus";
 import Navbar from "@/components/globals/landing-page/navbar";
 import GradualSpacing from "@/components/magicui/gradual-spacing";
 import SparklesText from "@/components/magicui/sparkles-text";
+import { useClerk, useUser } from "@clerk/nextjs";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const images = ["/images/bg1.jpg", "/images/bg2.jpg", "/images/bg3.jpg"];
 
 export default function Home() {
+  const { user } = useUser();
+  const { signOut } = useClerk();
   const router = useRouter();
+  const [isAuthorized, setIsAuthorized] = useState(false);
+
+  useEffect(() => {
+    // Redirect to sign-in if no user is logged in
+    if (!user) {
+      router.push("/");
+      return;
+    }
+
+    // Check if user is an admin
+    const isAdmin =
+      user?.emailAddresses[0]?.emailAddress === "coffeeprinceadmin@gmail.com";
+
+    if (isAdmin) {
+      signOut({ redirectUrl: "/" });
+    } else {
+      setIsAuthorized(true);
+    }
+  }, [user, router, signOut]);
+
+  // Render nothing while checking authorization status
+  if (!isAuthorized) {
+    return null;
+  }
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Navbar />
@@ -38,7 +66,10 @@ export default function Home() {
             text="Pure coffee, pure community, pure experience, because you can never
             have too much coffee in your life."
           />
-          <button onClick={() => router.push("/menu")} className="px-10 py-2 backdrop-blur-sm border bg-orange-300/50 border-orange-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+          <button
+            onClick={() => router.push("/menu")}
+            className="px-10 py-2 backdrop-blur-sm border bg-orange-300/50 border-orange-500/20 text-white mx-auto text-center rounded-full relative mt-4"
+          >
             <span className="text-lg">Buy Now &rarr;</span>
             <div className="absolute inset-x-0 h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-orange-500 to-transparent" />
           </button>
