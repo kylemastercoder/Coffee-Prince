3e3475c82b1195bf28e23587bef094038c4cc291
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2024-11-08 19:36:43 +0800

    fixed issues

--- src/app/admin/(pages)/layout.tsx
+++ src/app/admin/(pages)/layout.tsx
@@ -1,11 +1,24 @@
+"use client";
 
 import { cn } from "@/lib/utils";
 import React from "react";
 import SidebarAdmin from "./_components/sidebar-admin";
 import HeaderAdmin from "./_components/header-admin";
-
+import { useClerk, useUser } from "@clerk/nextjs";
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+  const { user } = useUser();
+  const { signOut } = useClerk();
+  if (!user) {
+    window.location.assign("/admin/sign-in");
+  }
+
+  if (user?.emailAddresses[0].emailAddress === "coffeeprinceadmin@gmail.com") {
+    window.location.assign("/admin/dashboard");
+  } else {
+    signOut({ redirectUrl: "/" });
+  }
+
   return (
     <div
       className={cn(
@@ -13,7 +26,10 @@ const AdminLayout = ({ children }: { children: React.ReactNode }) => {
         "h-screen"
       )}
     >
-      <SidebarAdmin imageUrl="https://github.com/shadcn.png" name="Billy Joe Dela Torre" />
+      <SidebarAdmin
+        imageUrl="https://github.com/shadcn.png"
+        name="Billy Joe Dela Torre"
+      />
       <div className="flex w-full flex-col">
         <HeaderAdmin />
         {children}
