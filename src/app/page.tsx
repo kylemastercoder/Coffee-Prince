489a19dceca44efbc5525e6bcf907a59ffbbdc18
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2024-11-08 21:42:31 +0800

    fixed null

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,12 +21,6 @@ export default function Home() {
   const [isAuthorized, setIsAuthorized] = useState(false);
 
   useEffect(() => {
-    // Redirect to sign-in if no user is logged in
-    if (!user) {
-      router.push("/");
-      return;
-    }
-
     // Check if user is an admin
     const isAdmin =
       user?.emailAddresses[0]?.emailAddress === "coffeeprinceadmin@gmail.com";
@@ -38,10 +32,6 @@ export default function Home() {
     }
   }, [user, router, signOut]);
 
-  // Render nothing while checking authorization status
-  if (!isAuthorized) {
-    return null;
-  }
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Navbar />
